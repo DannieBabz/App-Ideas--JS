e273c8730fc702bd6126617f439a9059f4c7b534
Babatunde Daniel <[similoluwababatunde@gmail.com]>
2022-10-11 17:35:12 +0100

    "countdown Timer"

--- Countdown Timer/script.js
+++ Countdown Timer/script.js
@@ -4,36 +4,53 @@ let day = document.querySelector(".days");
 let hour = document.querySelector(".hours");
 let minute = document.querySelector(".minutes");
 let second = document.querySelector(".seconds");
-
+day.innerHTML = 00;
+hour.innerHTML = 00;
+minute.innerHTML = 00;
+second.innerHTML = 00;
 //Onchange get the value of input
 //Perform the operation to subtract todays date and the set date
 //Input the individual values on each corresponding html elements
+// if (
+// 	day.innerHTML.length < 1 ||
+// 	hour.innerHTML.length < 1 ||
+// 	minute.innerHTML.length < 1 ||
+// 	second.innerHTML.length < 1
+// ) {
+// 	0 + day.innerHTML;
+// }
+dateVal.addEventListener("change", count);
+function count() {
+	let countdown = setInterval(
+		// 2022 - 10 - 07;
+		function setDate() {
+			const dateValue = new Date(dateVal.value).getTime();
+			let now = new Date().getTime();
+			let distance = dateValue - now;
 
-dateVal.addEventListener("change", setDate);
-// 2022 - 10 - 07;
+			let days = Math.floor(distance / (1000 * 60 * 60 * 24)),
+				hours = Math.floor(
+					(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+				),
+				minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+				seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-function setDate() {
-	const dateValue = new Date(dateVal.value).getTime();
-	let now = new Date().getTime();
-	let distance = dateValue - now;
+			day.innerHTML += days;
+			hour.innerHTML += hours;
+			minute.innerHTML += minutes;
+			second.innerHTML += seconds;
 
-	let days = Math.floor(distance / (1000 * 60 * 60 * 24)),
-		hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-		minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
-		seconds = Math.floor((distance % (1000 * 60)) / 1000);
+			if (distance < 0) {
+				day.innerHTML = 00;
+				hour.innerHTML = 00;
+				minute.innerHTML = 00;
+				seconds.innerHTML = 00;
+				clearInterval();
+			}
 
-	day.innerHTML += days;
-	hour.innerHTML += hours;
-	minute.innerHTML += minutes;
-	second.innerHTML += seconds;
-
-	if (distance == 0) {
-		day.innerHTML += 00;
-		hour.innerHTML += 00;
-		minute.innerHTML += 00;
-		seconds.innerHTML += 00;
-	}
-
-	console.log(dateValue);
-	console.log(now);
+			console.log(dateValue);
+			console.log(now);
+		},
+		1000
+	);
 }
